02dfbc99c364ae6e22fc13437b5ed9d4e3f91301
Maxim <maxmonsta@gmail.co>
2020-04-20 23:13:22 +0400

    multi

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main() {
     cin >> a >> b;
     cout << "A+B = " << a+b << '\n';
 	cout << "A-B = " << a-b << '\n';
-	cout << "A/B = " << a/b << '\n';
+	cout << "A*B = " << a(b << '\n';
     return 0;
 }
